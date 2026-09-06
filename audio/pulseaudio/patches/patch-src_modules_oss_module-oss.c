1055f3d8e680137cf5dff2f0d282e1cd9200714d
tsutsui <tsutsui@pkgsrc.org>
2026-09-06 14:11:05 +0000

    pulseaudio: fix excessive OSS playback latency on NetBSD
    NetBSD audio(4) recalculates the track buffer geometry when
    the sample format is changed. The module-oss currently sets
    the requested fragment configuration before negotiating the
    sample format, so the requested high-water mark is lost and
    reset to the full track buffer.
    
    With the 48 kHz 16-bit stereo format, this resulted in 65280-byte
    output buffer (34 x 1920 byte fragments), corresponding to
    ~340 ms of playback latency (should be ~40 ms) on my environment.
    
    Reapply the requested OSS fragment configuration after sample format
    negotiation on NetBSD, both when initially opening and when resuming
    the device.
    
    Bump PKGREVISION.

--- audio/pulseaudio/patches/patch-src_modules_oss_module-oss.c
+++ audio/pulseaudio/patches/patch-src_modules_oss_module-oss.c
@@ -1,6 +1,9 @@
-$NetBSD: patch-src_modules_oss_module-oss.c,v 1.4 2021/07/30 12:21:06 ryoon Exp $
+$NetBSD: patch-src_modules_oss_module-oss.c,v 1.5 2026/09/06 14:11:05 tsutsui Exp $
 
 Set DEVOSSAUDIO value to fix default device on NetBSD at least.
+
+Fix excessive OSS playback latency on NetBSD by reapplying the
+requested OSS fragment after sample format negotiation.
 
 --- src/modules/oss/module-oss.c.orig	2021-07-27 20:02:27.777868700 +0000
 +++ src/modules/oss/module-oss.c
@@ -13,3 +16,31 @@ Set DEVOSSAUDIO value to fix default device on NetBSD at least.
  
  struct userdata {
      pa_core *core;
+@@ -573,6 +573,14 @@ static int unsuspend(struct userdata *u)
+         goto fail;
+     }
+ 
++#ifdef __NetBSD__
++    if (u->nfrags >= 2 && u->frag_size >= 1)
++        if (pa_oss_set_fragments(u->fd, u->nfrags, u->orig_frag_size) < 0) {
++            pa_log_warn("Resume failed, couldn't restore fragment settings after format setup.");
++            goto fail;
++        }
++#endif
++
+     if (ioctl(u->fd, SNDCTL_DSP_GETBLKSIZE, &frag_size) < 0) {
+         pa_log_warn("SNDCTL_DSP_GETBLKSIZE: %s", pa_cstrerror(errno));
+         goto fail;
+@@ -1266,6 +1274,12 @@ int pa__init(pa_module*m) {
+     if (pa_oss_auto_format(fd, &ss) < 0)
+         goto fail;
+ 
++#ifdef __NetBSD__
++    if (nfrags >= 2 && frag_size >= 1)
++        if (pa_oss_set_fragments(fd, nfrags, frag_size) < 0)
++            goto fail;
++#endif
++
+     if (ioctl(fd, SNDCTL_DSP_GETBLKSIZE, &frag_size) < 0) {
+         pa_log("SNDCTL_DSP_GETBLKSIZE: %s", pa_cstrerror(errno));
+         goto fail;
