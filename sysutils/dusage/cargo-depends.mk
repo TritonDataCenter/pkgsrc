a279564263c3c12f3f80d514e0b1a34caed28ec0
pin <pin@pkgsrc.org>
2026-09-06 14:04:05 +0000

    sysutils/dusage: update to 0.4.1
     - add -p|--physical flag to filter out non-physical volumes - PR #63 by @jhoffmann
     - deps updates

--- sysutils/dusage/cargo-depends.mk
+++ sysutils/dusage/cargo-depends.mk
@@ -1,29 +1,33 @@
-# $NetBSD: cargo-depends.mk,v 1.7 2024/12/24 18:28:08 pin Exp $
+# $NetBSD: cargo-depends.mk,v 1.8 2026/09/06 14:04:05 pin Exp $
 
-CARGO_CRATE_DEPENDS+=	anstream-0.6.7
-CARGO_CRATE_DEPENDS+=	anstyle-1.0.8
-CARGO_CRATE_DEPENDS+=	anstyle-parse-0.2.1
+CARGO_CRATE_DEPENDS+=	anstream-1.0.0
+CARGO_CRATE_DEPENDS+=	anstyle-1.0.14
+CARGO_CRATE_DEPENDS+=	anstyle-parse-1.0.0
 CARGO_CRATE_DEPENDS+=	anstyle-query-1.0.0
-CARGO_CRATE_DEPENDS+=	anstyle-wincon-3.0.2
+CARGO_CRATE_DEPENDS+=	anstyle-wincon-3.0.11
 CARGO_CRATE_DEPENDS+=	autoclap-0.3.15
 CARGO_CRATE_DEPENDS+=	bitflags-2.3.3
 CARGO_CRATE_DEPENDS+=	byte-unit-4.0.19
 CARGO_CRATE_DEPENDS+=	cfg-if-1.0.0
 CARGO_CRATE_DEPENDS+=	cfg_aliases-0.2.1
-CARGO_CRATE_DEPENDS+=	clap-4.5.23
-CARGO_CRATE_DEPENDS+=	clap_builder-4.5.23
-CARGO_CRATE_DEPENDS+=	clap_lex-0.7.4
+CARGO_CRATE_DEPENDS+=	clap-4.6.6
+CARGO_CRATE_DEPENDS+=	clap_builder-4.6.6
+CARGO_CRATE_DEPENDS+=	clap_lex-1.0.0
 CARGO_CRATE_DEPENDS+=	colorchoice-1.0.0
 CARGO_CRATE_DEPENDS+=	colored-2.2.0
+CARGO_CRATE_DEPENDS+=	is_terminal_polyfill-1.70.2
 CARGO_CRATE_DEPENDS+=	lazy_static-1.4.0
-CARGO_CRATE_DEPENDS+=	libc-0.2.155
-CARGO_CRATE_DEPENDS+=	nix-0.29.0
+CARGO_CRATE_DEPENDS+=	libc-0.2.186
+CARGO_CRATE_DEPENDS+=	nix-0.31.3
+CARGO_CRATE_DEPENDS+=	once_cell_polyfill-1.70.2
 CARGO_CRATE_DEPENDS+=	serde-1.0.167
-CARGO_CRATE_DEPENDS+=	strsim-0.11.0
+CARGO_CRATE_DEPENDS+=	strsim-0.11.1
 CARGO_CRATE_DEPENDS+=	utf8-width-0.1.6
-CARGO_CRATE_DEPENDS+=	utf8parse-0.2.1
+CARGO_CRATE_DEPENDS+=	utf8parse-0.2.2
+CARGO_CRATE_DEPENDS+=	windows-link-0.2.1
 CARGO_CRATE_DEPENDS+=	windows-sys-0.48.0
 CARGO_CRATE_DEPENDS+=	windows-sys-0.52.0
+CARGO_CRATE_DEPENDS+=	windows-sys-0.61.2
 CARGO_CRATE_DEPENDS+=	windows-targets-0.48.1
 CARGO_CRATE_DEPENDS+=	windows-targets-0.52.0
 CARGO_CRATE_DEPENDS+=	windows_aarch64_gnullvm-0.48.0
