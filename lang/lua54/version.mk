607b3665c348a9539897136896ad5e970f223f0c
alnsn <alnsn@pkgsrc.org>
2026-09-06 14:32:00 +0000

    Update lang/lua54 to version 5.4.9.
    Lua 5.4.9
    
       That was the last release of Lua 5.4. Bugs reported later are most
       probably fixed in Lua 5.5.
    
    Lua 5.4.8 bugs:
    
        1. New metatable in an all-weak table can fool the GC.
           reported by jy l on 12 Jun 2025. existed since 5.3.0 (at least).
        2. Lua raises an error when given the option '--' without a script.
           reported by Jure Bagic on 04 Jul 2025. existed since 5.4.6.
        3. Constructors with nils can overflow counters during parsing.
           reported by Marco Schöpl on 26 Aug 2025. existed since 5.4 (at
           least).
        4. Possible memory leak due to finalizer being called with no stack
           space.
           reported by Sergey Bronnikov on 10 Dec 2025. existed since 5.4.3
           (at least).
    
    Lua 5.4.7 bugs:
    
        1. Wrong error message when multiline input is interrupted with
           end-of-file in the REPL.
           reported by Eric Oertel on 04 Jul 2024. existed since 5.3.
        2. Wrong code generation for indices with comparisons.
           reported by toxamin on 03 Aug 2024. existed since 5.4.0.
        3. Wrong limit for local variables in 16-bit systems.
           reported by Marcus Lund on 07 Sep 2024. existed since 5.4.0.
        4. Missing error status in panic function.
           reported by Alex Grund on 04 Jan 2025. existed since 5.4.4.
        5. An emergency GC can collect the __newindex of a metatable (if the
           metatable is a weak table) while that field is being used in a
           table update.
           reported by Xmilia Hermit on 09 Mar 2025. existed since 5.4.1 (at
           least).
        6. 'luaD_seterrorobj' should not raise errors, because it is called
           unprotected.
           reported by Xmilia Hermit on 10 Mar 2025. existed since 5.1.
        7. Message handler can be overwritten by a closing variable when
           closing a thread.
           reported by Xmilia Hermit on 10 Mar 2025.
        8. Wrong error message when using fields of string "_ENV".
           reported by Xmilia Hermit on 11 Mar 2025. existed since 5.4.0.

--- lang/lua54/version.mk
+++ lang/lua54/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.7 2024/06/28 05:35:11 wiz Exp $
+# $NetBSD: version.mk,v 1.8 2026/09/06 14:32:00 alnsn Exp $
 
-LUA_VERSION=	5.4.7
+LUA_VERSION=	5.4.9
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
